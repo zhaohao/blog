e869aff4e55e194ee2c4549de6521e08794e8622
zhaohao <hao@zhao.im>
2026-01-20 21:32:29 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -46,7 +46,7 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.About,
 		{
 			name: "Zmemos",
-			url: "https://memos.zlog.net", // Internal links should not include the base path, as it is automatically added
+			url: "https://zme.life", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
 	],
