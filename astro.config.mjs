f05f96223a56d3ba6be75c18288f2c545ce6431d
zhaohao <hao@zhao.im>
2026-01-25 13:33:13 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://stardust.hk/",
+	site: "https://zlog.net/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
